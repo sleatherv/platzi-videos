5ebae7cf68a0549f57eb679eb42a34c85a46917d
Sleather Vega <sleatherv28@hotmail.com>
2020-08-21 23:23:49 -0500

    Agregando categorias reutilizando componentes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -12,9 +12,25 @@ const App = () => (
     <div className="app">
         <Header />
         <Search />
-        <Categories>
+        <Categories title='Mi Lista'>
             <Carousel>
                 <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+        <Categories title='Tendencias'>
+            <Carousel>
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+        <Categories title='Originales de Platzi Video'>
+            <Carousel>
                 <CarouselItem />
                 <CarouselItem />
                 <CarouselItem />
