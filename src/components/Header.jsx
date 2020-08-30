0fde4d463b55871dddb7abda493a5812d763f0ea
Sleather Vega <sleatherv28@hotmail.com>
2020-08-29 21:15:47 -0500

    Manejando enlaces con el componente Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import user_icon from '../assets/static/user-icon.png';
 
 const Header = () => (
     <header className="header">
-        <img className="header__img" src={logo} alt="Platzi Video" />
+        <Link to="/">
+            <img className="header__img" src={logo} alt="Platzi Video" />
+        </Link>
         <div className="header__menu">
             <div className="header__menu--profile">
                 <img src={user_icon} alt="" />
                 <p>Perfil</p>
             </div>
             <ul>
-                <li><a href="/">Cuenta</a></li>
-                <li><a href="/">Cerrar Sesión</a></li>
+                <li><Link to="/register">Cuenta</Link></li>
+                <li><Link to="/login">Iniciar Sesión</Link></li>
             </ul>
         </div>
     </header>
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../assets/styles/components/Login.scss'
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
-
 
 const Login = () => (
 
@@ -30,7 +30,8 @@ const Login = () => (
 				</div>
             </section>
             <p className='login__container--register'>
-                No tienes ninguna cuenta <a href=''>Regístrate</a>
+                No tienes ninguna cuenta
+                <Link to='/register'>Regístrate</Link>
             </p>
         </section>
     </section>
