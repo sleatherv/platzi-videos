3ff628e16ff81531ed010c8280d892cd74b344d9
Sleather Vega <sleatherv28@hotmail.com>
2020-08-30 22:12:01 -0500

    Realizando el acction y reducer para agregar a favoritos

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export const setFavorite = payload => ({
+    type: 'SET_FAVORITE',
+    payload,
+});
+
+export default deleteFavorite = payload => ({
+    type: 'DELETE_FAVORITE',
+    payload
+});
