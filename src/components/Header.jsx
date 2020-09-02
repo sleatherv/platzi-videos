ca47a6fce721aebd41cf4457e38a49292dc6e872
Sleather Vega <sleatherv28@hotmail.com>
2020-09-01 21:35:39 -0500

    Agregando el manejo de Logout y validacion con proptypes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
 import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions/index';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import user_icon from '../assets/static/user-icon.png';
-import { object } from 'prop-types';
+
 
 const Header = (props) => {
     const { user } = props;
     const hasUser = Object.keys(user).length > 0;
+    const handleLogout = () => {
+        props.logoutRequest({});
+    }
     return (
         <header className="header">
             <Link to="/">
@@ -20,21 +25,34 @@ const Header = (props) => {
                     {hasUser ? <img src={gravatar(user.email)} alt={user.email} />
                         : <img src={user_icon} alt="" />
                     }
-
                     <p>Perfil</p>
                 </div>
                 <ul>
-                    <li><Link to="/register">Cuenta</Link></li>
-                    <li><Link to="/login">Iniciar Sesión</Link></li>
+                    {hasUser ?
+                        <li><Link to="/">{user.name}</Link></li>
+                        : null}
+                    {hasUser ?
+                        <li><Link to="#logout" onClick={handleLogout}>Cerrar Sesión</Link></li>
+                        : <li><Link to="/login">Iniciar Sesión</Link></li>}
                 </ul>
             </div>
         </header>
     );
 }
+
+Header.propTypes = {
+    user: propTypes.object,
+}
+
+
 const mapStateToProps = state => {
     return {
         user: state.user
     }
 }
 
-export default connect(mapStateToProps, null)(Header)
+const mapDispatchToProps = {
+    logoutRequest,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
