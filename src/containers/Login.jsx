480bdaa5e6d3bd939e3ca87faddbaf99e7324494
Sleather Vega <sleatherv28@hotmail.com>
2020-09-03 19:55:18 -0500

    Arreglando el boton del login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -59,8 +59,8 @@ const Login = (props) => {
 				</div>
                     </section>
                     <p className='login__container--register'>
-                        No tienes ninguna cuenta
-                <Link to='/register'>Regístrate</Link>
+                        No tienes ninguna cuenta {' '}
+                        <Link to='/register'>Regístrate</Link>
                     </p>
                 </section>
             </section>
