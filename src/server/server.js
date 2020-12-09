38116d6a62823f4fcd91506faf551731e3624294
Sleather Vega <sleatherv28@hotmail.com>
2020-12-08 21:31:18 -0500

    Serving react with express

--- src/server/server.js
+++ src/server/server.js
@@ -1,31 +1,43 @@
+/* eslint-disable global-require */
 import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
-
 
 dotenv.config();
 
 const { ENV, PORT } = process.env;
 const app = express();
 
-app.get('*', (req, res) => {
-    if (ENV === 'development') {
-        const webpackConfig = require('../../webpack.config');
-        const webpackDevMiddleware = require('webpack-dev-middleware');
-        const webpackHotMiddleware = require('webpack-hot-middleware');
-        const compiler = webpack(webpackConfig);
-        const serverConfig = { port: PORT, hot: true };
+if (ENV === 'development') {
+    console.log('Development config');
+    const webpackConfig = require('../../webpack.config');
+    const webpackDevMiddleware = require('webpack-dev-middleware')
+    const webpackHotMiddleware = require('webpack-hot-middleware');
+    const compiler = webpack(webpackConfig);
+    const serverConfig = { port: PORT, hot: true };
 
-        app.use(webpackDevMiddleware(compiler, serverConfig));
-        app.use(webpackHotMiddleware(compiler));
-    }
-    res.send({ hello: 'express' });
+    app.use(webpackDevMiddleware(compiler, serverConfig));
+    app.use(webpackHotMiddleware(compiler));
+
+}
+
+app.get('*', (req, res) => {
+    res.send(`
+  <!DOCTYPE html>
+    <html>
+      <head>
+        <link rel="stylesheet" href="assets/app.css" type="text/css">
+        <title>Platzi Video</title>
+      </head>
+      <body>
+        <div id="app"></div>
+        <script src="assets/app.js" type="text/javascript"></script>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Server running in http://localhost:3000');
-    }
+    if (err) console.log(err);
+    else console.log('Server running on port 3000');
 });
