ce563701e2f72c8c53ee111678660a843c30bb6c
Sleather Vega <sleatherv28@hotmail.com>
2020-12-08 19:20:13 -0500

    Using nodemon and Dotenv

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const { ENV, PORT } = process.env;
 const app = express();
 
 app.get('*', (req, res) => {
+    if (ENV === 'development') {
+        console.log('Development Config');
+    }
     res.send({ hello: 'express' });
 });
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
     if (err) {
         console.log(err);
     } else {
