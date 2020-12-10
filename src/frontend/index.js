7de3061b0556ddbcb46033dcad7b82c1f0bbf284
Sleather Vega <sleatherv28@hotmail.com>
2020-12-09 20:40:23 -0500

    adding react-router to client-side

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
 import { Provider } from 'react-redux';
 import { createStore, compose } from 'redux';
+import { createBrowserHistory } from 'history';
+
 import reducer from './reducers'
 import App from './routes/App';
 
@@ -172,12 +175,15 @@ const initialState = {
     ]
 };
 
+const history = createBrowserHistory();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, initialState, composeEnhancers());
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router history={history}>
+            <App />
+        </Router>
     </Provider>,
     document.getElementById('app'));
