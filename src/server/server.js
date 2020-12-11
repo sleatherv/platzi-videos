5be9be5225b83acd3ea030c159f6957fce241a4b
Sleather Vega <sleatherv28@hotmail.com>
2020-12-10 20:42:00 -0500

    Main function for SSR

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { renderRoutes } from 'react-router-config';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import { StaticRouter } from 'react-router-dom';
+import reducer from "../frontend/reducers";
+import initialState from '../frontend/initialState';
+import serverRoutes from '../frontend/routes/serverRoutes';
 
 dotenv.config();
 
@@ -9,35 +18,49 @@ const { ENV, PORT } = process.env;
 const app = express();
 
 if (ENV === 'development') {
-    console.log('Development config');
-    const webpackConfig = require('../../webpack.config');
-    const webpackDevMiddleware = require('webpack-dev-middleware')
-    const webpackHotMiddleware = require('webpack-hot-middleware');
-    const compiler = webpack(webpackConfig);
-    const serverConfig = { port: PORT, hot: true };
+  console.log('Development config');
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  const serverConfig = { port: PORT, hot: true };
 
-    app.use(webpackDevMiddleware(compiler, serverConfig));
-    app.use(webpackHotMiddleware(compiler));
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
 
 }
 
-app.get('*', (req, res) => {
-    res.send(`
-  <!DOCTYPE html>
-    <html>
-      <head>
-        <link rel="stylesheet" href="assets/app.css" type="text/css">
-        <title>Platzi Video</title>
-      </head>
-      <body>
-        <div id="app"></div>
-        <script src="assets/app.js" type="text/javascript"></script>
-      </body>
-    </html>
-  `);
-});
+const setResponse = (html) => {
+  return (`
+    <!DOCTYPE html>
+      <html>
+        <head>
+          <link rel="stylesheet" href="assets/app.css" type="text/css">
+          <title>Platzi Video</title>
+        </head>
+        <body>
+          <div id="app">${html}</div>
+          <script src="assets/app.js" type="text/javascript"></script>
+        </body>
+      </html>
+    `);
+}
+
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>
+  );
+  res.send(setResponse(html));
+};
+
+app.get('*', renderApp);
 
 app.listen(PORT, (err) => {
-    if (err) console.log(err);
-    else console.log('Server running on port 3000');
+  if (err) console.log(err);
+  else console.log('Server running on port 3000');
 });
