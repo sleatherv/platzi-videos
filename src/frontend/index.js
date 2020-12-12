3482f8b8848d83b73993cdd1714c8a896c897721
Sleather Vega <sleatherv28@hotmail.com>
2020-12-12 12:28:02 -0500

    Using Hydrate and set Redux status from Express

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -4,17 +4,17 @@ import { Router } from 'react-router';
 import { Provider } from 'react-redux';
 import { createStore, compose } from 'redux';
 import { createBrowserHistory } from 'history';
-import initialState from './initialState';
 import reducer from './reducers'
 import App from './routes/App';
 
 
 const history = createBrowserHistory();
+const preloadedState = window.__PRELOADED_STATE__;
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, preloadedState, composeEnhancers());
 
-const store = createStore(reducer, initialState, composeEnhancers());
-
-ReactDOM.render(
+delete window.__PRELOADED_STATE__;
+ReactDOM.hydrate(
     <Provider store={store}>
         <Router history={history}>
             <App />
