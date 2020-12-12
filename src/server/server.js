076e7af8eed1dc12b903832272dd2b10fcd82d6d
Sleather Vega <sleatherv28@hotmail.com>
2020-12-12 13:05:12 -0500

    Configured our server to produccion

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
 import React from 'react';
+import helmer from 'helmet';
 import { renderToString } from 'react-dom/server';
 import { renderRoutes } from 'react-router-config';
 import { Provider } from 'react-redux';
@@ -11,6 +12,7 @@ import { StaticRouter } from 'react-router-dom';
 import reducer from "../frontend/reducers";
 import initialState from '../frontend/initialState';
 import serverRoutes from '../frontend/routes/serverRoutes';
+import helmet from 'helmet';
 
 dotenv.config();
 
@@ -20,7 +22,7 @@ const app = express();
 if (ENV === 'development') {
   console.log('Development config');
   const webpackConfig = require('../../webpack.config');
-  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackDevMiddleware = require('webpack-dev-middleware');
   const webpackHotMiddleware = require('webpack-hot-middleware');
   const compiler = webpack(webpackConfig);
   const serverConfig = { port: PORT, hot: true };
@@ -28,6 +30,11 @@ if (ENV === 'development') {
   app.use(webpackDevMiddleware(compiler, serverConfig));
   app.use(webpackHotMiddleware(compiler));
 
+} else {
+  app.use(express.static(`${__dirname}/public`));
+  app.use(helmet());
+  app.use(helmet.permittedCrossDomainPolicies());
+  app.disable('x-powered-by');
 }
 
 const setResponse = (html, preloadedState) => {
