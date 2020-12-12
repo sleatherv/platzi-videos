2d96b19dffa63c41097aaf510232c75a067da96f
Sleather Vega <sleatherv28@hotmail.com>
2020-12-12 14:16:35 -0500

    Buld optimization - webpack terser

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionWebPackPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 require('dotenv').config();
 
@@ -21,6 +23,10 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx']
     },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
     module: {
         rules: [
             {
@@ -29,12 +35,6 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
-            },
-            {
-                test: /\.html$/,
-                use: [{
-                    loader: 'html-loader'
-                }]
             },
             {
                 test: /\.(s*)css$/,
@@ -64,6 +64,11 @@ module.exports = {
     plugins: [
         isDev ? new webpack.HotModuleReplacementPlugin() :
             () => { },
+        isDev ? () => { } :
+            new CompressionWebPackPlugin({
+                test: /\.js$|\.css$/,
+                filename: '[path].gz'
+            }),
         new MiniCssExtractPlugin({
             filename: 'assets/app.css',
         }),
