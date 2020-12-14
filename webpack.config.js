c7a6acda374cf10f5edb0adc83d1b3c4711135eb
Sleather Vega <sleatherv28@hotmail.com>
2020-12-13 20:39:32 -0500

    Fixed wepbpack file es-lint

--- webpack.config.js
+++ webpack.config.js
@@ -12,97 +12,97 @@ const isDev = (process.env.ENV === 'development');
 const entry = ['./src/frontend/index.js'];
 
 if (isDev) {
-    entry.push('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true');
+  entry.push('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true');
 }
 module.exports = {
-    entry,
-    mode: process.env.ENV,
-    output: {
-        path: path.resolve(__dirname, 'src/server/public'),
-        filename: isDev ? 'assets/app.js' : 'assets/app-[hash].js',
-        publicPath: '/'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin()],
-        splitChunks: {
-            chunks: 'async',
-            name: true,
-            cacheGroups: {
-                vendors: {
-                    name: 'vendors',
-                    chunks: 'all',
-                    reuseExistingChunk: true,
-                    priority: 1,
-                    filename: isDev ? 'assets/vendor.js' : 'assets/vendor-[hash].js',
-                    enforce: true,
-                    test(module, chunks) {
-                        const name = module.nameForCondition && module.nameForCondition();
-                        return chunks.some(chunk => chunk.name !== 'vendors' && /[\\/]node_modules[\\/]/.test(name));
-                    },
-                },
-            },
+  entry,
+  mode: process.env.ENV,
+  output: {
+    path: path.resolve(__dirname, 'src/server/public'),
+    filename: isDev ? 'assets/app.js' : 'assets/app-[hash].js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+    splitChunks: {
+      chunks: 'async',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          name: 'vendors',
+          chunks: 'all',
+          reuseExistingChunk: true,
+          priority: 1,
+          filename: isDev ? 'assets/vendor.js' : 'assets/vendor-[hash].js',
+          enforce: true,
+          test(module, chunks) {
+            const name = module.nameForCondition && module.nameForCondition();
+            return chunks.some(chunk => chunk.name !== 'vendors' && /[\\/]node_modules[\\/]/.test(name));
+          },
         },
+      },
     },
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: "eslint-loader",
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+        },
+        'css-loader',
+        'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|gif|jpg)$/,
+        use: [
+          {
+            'loader': 'file-loader',
+            'options': {
+              name: 'assets/[hash].[ext]',
             },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.(s*)css$/,
-                use: [{
-                    loader: MiniCssExtractPlugin.loader,
-                },
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(png|gif|jpg)$/,
-                use: [
-                    {
-                        'loader': 'file-loader',
-                        'options': {
-                            name: 'assets/[hash].[ext]'
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    devServer: {
-        historyApiFallback: true,
-    },
-    plugins: [
-        isDev ? new webpack.HotModuleReplacementPlugin() :
-            () => { },
-        isDev ? () => { } :
-            new CompressionWebPackPlugin({
-                test: /\.js$|\.css$/,
-                filename: '[path].gz'
-            }),
-        isDev ? () => { } :
-            new ManifestPlugin(),
-        new MiniCssExtractPlugin({
-            filename: isDev ? 'assets/app.css' : 'assets/app-[hash].css',
-        }),
-        isDev ? () => { } :
-            new CleanWebpackPlugin({
-                cleanOnceBeforeBuildPatterns: path.resolve(__dirname, 'src/server/public')
-            })
-    ]
-}
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    isDev ? new webpack.HotModuleReplacementPlugin() :
+      () => { },
+    isDev ? () => { } :
+      new CompressionWebPackPlugin({
+        test: /\.js$|\.css$/,
+        filename: '[path].gz',
+      }),
+    isDev ? () => { } :
+      new ManifestPlugin(),
+    new MiniCssExtractPlugin({
+      filename: isDev ? 'assets/app.css' : 'assets/app-[hash].css',
+    }),
+    isDev ? () => { } :
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: path.resolve(__dirname, 'src/server/public'),
+      }),
+  ],
+};
